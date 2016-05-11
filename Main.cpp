9cc189aa46894c1dc87bd7219307deda34b54d99
nero19960329 <wangzhao2013tsinghua@gmail.com>
2016-05-11 16:47:10 +0800

    modify output

--- Main.cpp
+++ Main.cpp
@@ -11,16 +11,20 @@ int main() {
 	time_t begin, end;
 	begin = clock();
 
+	cout << "============================================" << endl;
 	cout << "Initializing tane algorithm . . ." << endl;
 	Tane tane = Tane("./data.txt");
 	cout << "Initialize ok!" << endl;
+	cout << "============================================" << endl;
 	cout << "Running algorithm . . ." << endl;
 	tane.run(fout);
+	cout << "============================================" << endl;
 	cout << "Number of functional depedencies : " << tane.getCount() << endl;
 	cout << "Get more detail in result.txt" << endl;
 
 	end = clock();
 	cout << "Runtime : " << double(end - begin) / CLOCKS_PER_SEC << endl;
+	cout << "============================================" << endl;
 
 	fout.flush();
 	fout.close();
